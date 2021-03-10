2b4069bf3226e727b91caa07f4b949481cb4fc5d
Bao Le <bao.le@hesmantech.com>
2021-03-10 11:39:10 +0700

    Bao Le: converted to Spring Boot 2

---
+++ src/main/java/com/mkyong/web/controller/SpringBootConverter.java
@@ -0,0 +1,11 @@
+package com.mkyong.web.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConverter {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConverter.class, args);
+    }
+}
